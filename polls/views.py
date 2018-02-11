c0fa0fbf1fb3960a42a381bb1a2f65bba8eb35d3
Алексей Белов <Alenorze@gmail.com>
2018-02-11 02:10:57 +0200

    FBV Views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import HttpResponse
 
 
-def index(request):
+def index(request, question_id):
     return HttpResponse("<h1>Hello, its last chance...</h2>")
+
+def detail(request, question_id):
+    return HttpResponse("U r looking for results %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("U r voting %s" % question_id)
+    
